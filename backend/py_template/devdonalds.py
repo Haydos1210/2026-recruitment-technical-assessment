c00cffbef6c9450d5a525f101583ea373a412365
Hayden <z5592258@ad.unsw.edu.au>
2026-02-13 02:42:07 +1100

    finished devsov recruitment application tasks

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import List, Dict, Union
 from flask import Flask, request, jsonify
 import re
-
+RECIPEAMT = 1
 # ==== Type Definitions, feel free to add or modify ===========================
 @dataclass
 class CookbookEntry:
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,24 +43,113 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+	result = re.sub(r"[_-]", r" ", recipeName)
+	result = re.sub(r"[^a-zA-Z ]", r"", result)
+	result = result.split(" ")
+	newRecipeName = []
+
+	for word in result:
+		word = word.lower().capitalize()
+		newRecipeName.append(word)
+	newRecipeName = " ".join(newRecipeName)
+
+	if len(recipeName) <= 0:
+		return None
+	return newRecipeName
 
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	entryData = request.get_json()
+	entryDataClass = entryData["type"]
+	if entryDataClass != "recipe" and entryDataClass != "ingredient":
+		return jsonify({}), 400
+	
+	if entryDataClass == "ingredient":
+		if entryData["cookTime"] < 0:
+			return jsonify({}), 400
+		
+		if "requiredItems" in entryData:
+			return jsonify({}), 400
+
+	if entryDataClass == "recipe":
+		if "cookTime" in entryData:
+			return jsonify({}), 400
+
+		# Checking for duplicate required items
+		itemList = []
+		for reqItem in entryData["requiredItems"]:
+			if reqItem["name"] not in itemList:
+				itemList.append(reqItem["name"])
+			else:
+				return jsonify({}), 400
+
+	if entryData["name"] in cookbook:
+		return jsonify({}), 400
+	
+	cookbook[entryData["name"]] = entryData
+	return jsonify({}), 200
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	ingredientList = {}
+	totalCookTime = 0
+
+	# recursively goes through each requiredItem for a recipe
+	def validSummary(name, multiplier):
+		nonlocal totalCookTime
+		nonlocal ingredientList
+
+		if name not in cookbook:
+			return False
+
+		entry = cookbook[name]
+		if entry["type"] == "ingredient":
+			if name not in ingredientList:
+				ingredientList[name] = multiplier
+			else:
+				ingredientList[name] += multiplier
+
+			totalCookTime += multiplier * entry["cookTime"]
+			return True
+		elif entry["type"] != "recipe":
+			return False
+		
+		for reqItem in entry["requiredItems"]:
+			reqItemName, reqItemAmt = reqItem["name"], reqItem["quantity"]
+			if not validSummary(reqItemName, reqItemAmt * multiplier):
+				return False
+
+		return True
+
+	recipeName = request.args.get("name")
+	if recipeName not in cookbook:
+		return jsonify({}), 400
+
+	entry = cookbook[recipeName]
+	if entry["type"] == "ingredient":
+		return jsonify({}), 400
+	if not validSummary(recipeName, RECIPEAMT):
+		return jsonify({}), 400
+		
+	finalIngredientList = []
+	for name, quantity in ingredientList.items():
+		finalIngredientList.append({
+			"name": name,
+			"quantity": quantity
+		})
+
+	return jsonify({
+		"name": recipeName,
+		"cookTime": totalCookTime,
+		"ingredients": finalIngredientList
+	}), 200
+
 
 
 # =============================================================================
